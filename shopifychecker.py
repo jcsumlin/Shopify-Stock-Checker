547f253f02c985cf0d6443685aebe17df34b0d97
Chat Sumlin <jcsumlin@gmail.com>
2021-02-11 14:54:21 -0500

    Updated to 3.6 and added better support for alerts

--- shopifychecker.py
+++ shopifychecker.py
@@ -1,100 +1,111 @@
 # -*- coding: utf-8 -*-
-#!/python2.7
-#Created by aj nicolas
-import requests, crayons
-from bs4 import BeautifulSoup
+# Created by aj nicolas
+from datetime import datetime
 
-#While true to get a actual link
-def linkfriendly():
-    global url
-    global r
-    global soup
+import requests
+import json
+import os
+from dateutil import parser
 
-    while True:
-        #Gets user shopify link
-        try:
-            url = raw_input(crayons.cyan('PASTE LINK HERE: '))
-            headers ={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36'
-                    '(KHTML, like Gecko) Chrome/56.0.2924.28 Safari/537.36'}
+# While true to get a actual link
+from loguru import logger
 
-            r = requests.get(url+'.xml' ,headers=headers )
-            soup = BeautifulSoup(r.content, 'html.parser')
-            break
-            # Handles exceptions
-        except (requests.exceptions.MissingSchema,requests.exceptions.InvalidURL,requests.exceptions.ConnectionError,
-            requests.exceptions.InvalidSchema,NameError) as e:
-            print crayons.red('link no bueno!')
 
-#Grabs handle text
-def grabhandle():
-    for handL in soup.findAll("handle"):
-        return 'Handle: ' + handL.text
-
-def grabdate():
-    for created in soup.findAll("created-at"):
-        return 'created: ' + created.text
-
-def grabsku():
-    for sku in soup.findAll("sku"):
-        return 'sku: ' + sku.text
-
-def grabprice():
-    for price in soup.findAll("price"):
-        return 'Price: ' + price.text
-
-#Parses stock,sz name, and variants from shopify site
-def grabszstk():
-    sz = []
-    for size in soup.findAll("title")[1:]:
-        # find text then append to a list
-        sz.append(size)
- 
-    stk = []
-    for stock in soup.findAll("inventory-quantity"):
-        stk.append(stock)
-
-    variants = []
-    for variant in soup.findAll("id")[1:]:
-        variants.append(variant)
-    #Gets the total
-    total = []
-    for stock in soup.findAll("inventory-quantity"):
-        total.append(int(stock.text))
-    
-    # formats the data
-    fmt = '{:<5}{:<13}{:<10}{}'
-    fmat = '{:<5}{:<13}{}'
-
-    # zips the for lists together
-    if len(stk) > 0:
-        print(fmt.format('', 'size', 'stock', 'variant'))
-        for i, (sz, stk, variants) in enumerate(zip(sz, stk, variants)):
-            print(fmt.format(i, sz.text, stk.text, variants.text))
-        print 'TOTAL STOCK:', sum(total)
-
-    #if stock wasn't found
-    else:
-        print crayons.red('STOCK WAS NOT FOUND')
-        print(fmat.format('', 'size','variant'))
-        for i, (sz,variants) in enumerate(zip(sz,variants)):
-            print(fmat.format(i, sz.text, variants.text))
-
-#Also bad formatting
-def formattext():
-    print '--' * 38
-    print crayons.blue(url)
-    print crayons.yellow('Press cmd + double click link to go to link!')
+def fetch_info(url=""):
     try:
-        print grabhandle() + ' | ' + grabdate() + ' \n' + crayons.green(grabprice()) + ' \n' + grabsku()
-        print ' '*38
-        grabszstk()
-        print crayons.yellow('Press ctrl + z to exit')
-    except TypeError:
-        print crayons.red("Try copying everything before the '?variant' \n or before the '?' in the link!".upper())
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36'
+                                 '(KHTML, like Gecko) Chrome/56.0.2924.28 Safari/537.36'}
 
-#While true statment for multiple link checks!
-while True:
-    if linkfriendly() is True:
-        print linkfriendly()
-    elif formattext() is True:
-        print formattext()
+        r = requests.get(url + '.js', headers=headers)
+        r1 = requests.get(url + '.json', headers=headers) # Need the additional data from this extension
+
+    except (requests.exceptions.MissingSchema, requests.exceptions.InvalidURL, requests.exceptions.ConnectionError,
+            requests.exceptions.InvalidSchema, NameError) as e:
+        logger.error(f"The link \"{url}\" is invalid. Please check its formatting.")
+        return False
+    logger.success(f"Successfully retrieved product data from lin {url}")
+    data_js = json.loads(r.content)
+    data_json = json.loads(r1.content)
+    data_js["updated_at"] = data_json["product"]["updated_at"]
+    for var in data_json["product"]["variants"]:
+        i = 0
+        for variant_js in data_js["variants"]:
+            if var["id"] == variant_js["id"]:
+                data_js["variants"][i]["updated_at"] = var["updated_at"]
+            i += 1
+    return data_js
+
+
+def parse_product(data):
+    product = {
+        "title": data["title"],
+        "updated_at": parser.parse(data["updated_at"]),
+        "variants": [],
+    }
+    for item in data["variants"]:
+        variant = {
+            "id": item["id"],
+            "name": item["name"],
+            "price": "$" + str(item["price"] / 100),
+            "can_purchase": bool(item["available"]),
+            "updated_at": parser.parse(item["updated_at"]),  # 2021-02-01T22:18:00-05:00
+            "inventory": int(item["inventory_quantity"] if "inventory_quantity" in item else 0),
+            "image": item["featured_image"]["src"] if "featured_image" in item else "",
+            "link": data["url"]
+        }
+        product["variants"].append(variant)
+    return product
+
+
+def post_to_discord(item, url=""):
+    if "DISCORD_WEBHOOK_URL" in os.environ:
+        data = {
+            "username": "Shopify Stock Checker",
+            "avatar_url": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-download.com%2Fwp-content%2Fuploads%2F2016%2F10%2FShopify_logo_icon.png&f=1&nofb=1",
+            "embeds": [
+                {
+                    "title": item["name"] + " IN STOCK!",
+                    "type": "rich",
+                    "description": "The item you are watching is back in stock and available in the merchant's store! Order yours before they run out!"
+                                   f"\n\n**Price**: {item['price']}\n"
+                                   f"**Inventory Available:** {item['inventory']} ",
+                    "url": url,
+                    "color": 1879160,
+                    "image": {
+                        "url": item["image"]
+                    }
+                }
+            ]
+        }
+        headers = {
+            "Content-Type": "application/json"
+        }
+        r= requests.post(os.environ["DISCORD_WEBHOOK_URL"], json=data, headers=headers)
+        if r.status_code not in [200,204]:
+            logger.error("There was a problem posting to your discord webhook")
+        else:
+            logger.success(f"Successfully posted to Discord webhook! Item {item['name']}")
+
+
+if __name__ == "__main__":
+    with open("./links.json", "r") as links:
+        data = links.read()
+        obj = json.loads(data)
+    for index in range(0, len(obj)):
+        shopify_product = obj[index]
+        page = fetch_info(shopify_product["link"])
+        if page is not False:
+            data = parse_product(page)
+            if shopify_product["alerted_on"] is None or parser.parse(shopify_product["alerted_on"]) < data["updated_at"]:
+                alerted = False
+                for variant in data["variants"]:
+                    if variant["can_purchase"]:
+                        alerted = True
+                        post_to_discord(variant, url=shopify_product["link"] + "?variant=" + str(variant["id"]))
+                if alerted:
+                    now = datetime.utcnow()
+                    obj[index]["alerted_on"] = now.strftime("%Y-%m-%dT%H:%M:%S%z+00:00")
+
+    with open('links.json', 'w') as f:
+        json.dump(obj, f)
+
